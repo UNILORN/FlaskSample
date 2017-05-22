3b26ee5c9c14797535680a921876d07fafe12376
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-05-22 23:43:29 +0900

    update

--- FlaskSample.py
+++ FlaskSample.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+# coding=utf-8
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
 
 
+# ルーティングと表示
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+# 値渡し HTMLテンプレート
 @app.route('/test/<name>')
-def yahho(name=None):
+def html_test(name=None):
+    return render_template('index.html', message=name)
+
+
+# session
+@app.route('/session/<name>')
+def session_test(name=None):
+    session['session'] = name
+    return render_template('index.html', message=name)
+
+
+# session show
+@app.route('/session/show')
+def sessionshow_test():
+    name = session['session']
     return render_template('index.html', message=name)
 
 
