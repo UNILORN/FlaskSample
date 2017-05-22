d0d9ec8fb80723a7b216f67cd2f4735cba0593eb
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-05-22 23:25:45 +0900

    readme

--- FlaskSample.py
+++ FlaskSample.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/test')
+def yahho():
+    return render_
+
 
 if __name__ == '__main__':
     app.run()
---
+++ README.md
@@ -0,0 +1,6 @@
+# Title
+
+## Start
+
+`python FlaskSample.py`
+
